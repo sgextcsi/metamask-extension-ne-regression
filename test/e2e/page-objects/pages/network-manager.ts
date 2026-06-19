1371be1d5a80e8d7549d8ded0a54237eac4bcd60
sgextcsi <211475849+sgextcsi@users.noreply.github.com>
2026-06-19 22:49:07 +0530

    token_send_change_add

--- test/e2e/page-objects/pages/network-manager.ts
+++ test/e2e/page-objects/pages/network-manager.ts
@@ -45,7 +45,7 @@ class NetworkManager {
     `[data-testid="${networkName}"]`;
 
   private readonly networkManagerCloseButton =
-    '[data-testid="modal-header-close-button"]';
+    '[data-testid="modal-header-close-button"], [aria-label="Close"]';
 
   private readonly networkManagerSelectAllButton =
     '[data-testid="network-manager-select-all"]';
@@ -78,14 +78,6 @@ class NetworkManager {
     await this.driver.clickElementAndWaitToDisappear(
       this.networkManagerCloseButton,
     );
-  }
-
-  async selectTab(tabName: string): Promise<void> {
-    console.log(`Selecting tab: ${tabName}`);
-    await this.driver.clickElement({
-      text: tabName,
-    });
-    await this.waitForCategoryContent(tabName);
   }
 
   async selectNetworkByNameWithWait(networkName: string): Promise<void> {
